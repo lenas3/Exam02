d19133f3c51a09e51b11b98f06de05c47bf9c31b
Aleyna <sayaleyna27@gmail.com>
2025-09-09 03:54:11 +0300

    Add files via upload

---
+++ exam02/inter.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+char *non_duplicate(char *str)
+{
+    int i;
+    int j;
+    char letter;
+    char new[100];
+    i = 0;
+    j = 0;
+    while(str[i]  != '\0')
+    {
+        letter = str[i];
+        if(letter == str[i])
+        {
+
+        }
+    }
+}
