bef2857a95231409940414f77f4d26681f3f3e67
Aleyna <sayaleyna27@gmail.com>
2025-09-23 16:56:28 +0300

    Level 2 hidenp Sorusu

---
+++ Exam/Level2/hidenp.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    if(argc == 3)
+    {
+        int i = 0;
+        int j = 0;
+        char *str1 = argv[1];
+        char *str2 = argv[2];
+
+        while(str1[i] && str2[j])
+        {
+            if(str1[i] == str2[j])
+                i++;
+            j++;
+        }
+        if(str1[i] == '\0')
+            write(1, "1", 1);
+        else 
+            write(1, "0", 1);
+    }
+    write(1, "\n", 1);
+    return 0;
+}
