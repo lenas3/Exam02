6f93741cd9e4a7da7640f9a9eab12f3b96c3e33b
Aleyna <sayaleyna27@gmail.com>
2025-09-23 20:44:55 +0300

    Add files via upload

---
+++ Exam/Level4/flood_fill/flood_fill.h
@@ -0,0 +1,14 @@
+
+#ifndef FLOOD_FILL_H
+# define FLOOD_FILL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+
+typedef struct s_point
+{
+  int           x;
+  int           y;
+}           t_point;
+void flood_fill(char **tab, t_point size, t_point begin);
+#endif
